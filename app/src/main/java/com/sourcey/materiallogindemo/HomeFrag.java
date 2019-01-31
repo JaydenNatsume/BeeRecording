4795edbe3f6175707879bf680d1de12cc2e71568
UTLight <YULight@bupt.edu.cn>
2019-01-31 15:07:06 -0600

    尝试整合登录和主页（失败，待回退）

---
+++ app/src/main/java/com/sourcey/materiallogindemo/HomeFrag.java
@@ -0,0 +1,9 @@
+package com.sourcey.materiallogindemo;
+
+import android.support.v4.app.Fragment;
+
+public class HomeFrag extends Fragment {
+
+
+
+}
---
+++ app/src/main/java/com/sourcey/materiallogindemo/TransactionFrag.java
@@ -0,0 +1,6 @@
+package com.sourcey.materiallogindemo;
+
+import android.support.v4.app.Fragment;
+
+public class TransactionFrag extends Fragment {
+}
