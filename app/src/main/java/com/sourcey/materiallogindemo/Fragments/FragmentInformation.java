f494e06b901b50bfd1eec6c00b09f958456ce6ff
Jayden <YULight@bupt.edu.cn>
2019-02-26 12:15:43 -0600

    Add update username page.

--- app/src/main/java/com/sourcey/materiallogindemo/Fragments/FragmentInformation.java
+++ app/src/main/java/com/sourcey/materiallogindemo/Fragments/FragmentInformation.java
@@ -1,12 +1,15 @@
 package com.sourcey.materiallogindemo.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.sourcey.materiallogindemo.Information.UpdateUsername;
 import com.sourcey.materiallogindemo.R;
 
 public class FragmentInformation extends Fragment {
@@ -15,6 +18,15 @@ public class FragmentInformation extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_information, container, false);
+
+        EditText username_edit = view.findViewById(R.id.information_name);
+        username_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent username_intent = new Intent(getActivity(), UpdateUsername.class);
+                startActivity(username_intent);
+            }
+        });
 
         return view;
     }
---
+++ app/src/main/java/com/sourcey/materiallogindemo/Information/UpdateUsername.java
@@ -0,0 +1,29 @@
+package com.sourcey.materiallogindemo.Information;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.sourcey.materiallogindemo.R;
+
+public class UpdateUsername extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_username);
+
+        EditText username_edit = findViewById(R.id.information_name);
+        username_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Save and update username to database.
+
+
+                // After save the change of username, back to last Fragment.
+                UpdateUsername.this.finish();
+            }
+        });
+    }
+}
